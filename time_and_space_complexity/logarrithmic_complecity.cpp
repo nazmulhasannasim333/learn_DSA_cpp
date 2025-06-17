69931b68a536db1b4f4e111cd6e15aca0ef30385
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 11:49:41 +0600

    Fix formatting in logarithmic complexity program and update summary comment

--- time_and_space_complexity/logarrithmic_complecity.cpp
+++ time_and_space_complexity/logarrithmic_complecity.cpp
@@ -6,7 +6,7 @@ int main()
     int n;    // ---> O(1)
     cin >> n; // ---> O(1)
 
-    for (int i = 1; i <= n; i*=2) // ---> O(log n)
+    for (int i = 1; i <= n; i *= 2) // ---> O(log n)
     {
         cout << i << endl;
     }
@@ -18,4 +18,4 @@ int main()
 // If increment by multiplying, so it is logarithmic complexity = O(log n)
 // If decrement by dividing, so it is logarithmic complexity = O(log n)
 
-
+// The overall time complexity of this code is O(log n) + O(1) + O(1) = O(log n).
