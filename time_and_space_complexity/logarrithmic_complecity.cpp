5c544a311201e1deb66f800ac0d91cfb9cafe557
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 11:48:42 +0600

    Add implementation of logarithmic complexity program with output

---
+++ time_and_space_complexity/logarrithmic_complecity.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;    // ---> O(1)
+    cin >> n; // ---> O(1)
+
+    for (int i = 1; i <= n; i*=2) // ---> O(log n)
+    {
+        cout << i << endl;
+    }
+
+    return 0; // ---> O(1)
+}
+
+// Summery
+// If increment by multiplying, so it is logarithmic complexity = O(log n)
+// If decrement by dividing, so it is logarithmic complexity = O(log n)
+
+
