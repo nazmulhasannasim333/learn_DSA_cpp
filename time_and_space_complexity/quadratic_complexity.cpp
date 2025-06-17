9d7ce65dd4b359b57a6006b2ad50de4e3839d6f6
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 22:02:56 +0600

    Refactor code structure for improved readability and maintainability

--- time_and_space_complexity/quadratic_complexity.cpp
+++ time_and_space_complexity/quadratic_complexity.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main()
 {
-
+    int n;                       // ----> O(1)
+    cin >> n;                    // ----> O(1)
+    for (int i = 1; i <= n; i++) // ----> O(n)
+    {
+        for (int j = 1; j <= n; j++) // ----> O(n)
+        {
+            cout << "Hello" << endl;
+        }
+    }
+    {
+    }
     return 0;
 }
