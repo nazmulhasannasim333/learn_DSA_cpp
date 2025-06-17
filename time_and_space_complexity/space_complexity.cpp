ebff95488c1c4cc785ff197604997889b8e4a27b
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 22:52:47 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ time_and_space_complexity/space_complexity.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n]; // ----> O(n) space complexity
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> a[i];
+    }
+    return 0;
+}
