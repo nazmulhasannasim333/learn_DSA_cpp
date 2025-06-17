ea028532fb7f782a1c171cf8a901737a3195c743
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 11:08:04 +0600

    Refactor linear complexity program and update output file; add VSCode settings

--- time_and_space_complexity/linear_complexity.cpp
+++ time_and_space_complexity/linear_complexity.cpp
@@ -3,11 +3,13 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 1; i <= n; i++)
+    int n;                       //----------> O(1)
+    cin >> n;                    //----------> O(1)
+    for (int i = 1; i <= n; i++) //----------> O(n)
     {
         cout << i << " ";
     }
-    return 0;
+    return 0; //----------> O(1)
 }
+
+// The overall time complexity of this code is O(n).
