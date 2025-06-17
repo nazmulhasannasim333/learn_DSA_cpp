867b0b2342a58f58ba5d67113e4ec59ff1b394d7
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 19:29:29 +0600

    Enhance code readability by adding complexity annotations and correcting summary comments

--- time_and_space_complexity/sqrt_complexity.cpp
+++ time_and_space_complexity/sqrt_complexity.cpp
@@ -3,11 +3,16 @@ using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-    for (int i = 1; i < sqrt(n); i++)
+    int n;                            // ----> O(1)
+    cin >> n;                         // ----> O(1)
+    for (int i = 1; i < sqrt(n); i++) // ----> O(sqrt(n))
     {
         cout << i << endl;
     }
-    return 0;
+    return 0; // ----> O(1)
 }
+
+// Summery of complexities:
+// O(1) + O(1) + O(sqrt(n)) + O(1) = O(sqrt(n))
+
+// The overall time complexity of this code is O(sqrt(n)).
