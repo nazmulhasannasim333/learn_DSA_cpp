3cdfb79acd0a62832f79d61411e44001060eed4d
Nasim <mdnasimhosen333@gmail.com>
2025-06-17 11:17:57 +0600

    Add loops to output numbers and update time complexity analysis

--- time_and_space_complexity/linear_complexity.cpp
+++ time_and_space_complexity/linear_complexity.cpp
@@ -9,7 +9,26 @@ int main()
     {
         cout << i << " ";
     }
+    cout << endl;
+    for (int i = 1; i <= n / 2; i++) //----------> O(n)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+    for (int i = 1; i <= n; i += 2) //----------> O(n)
+    {
+        cout << i << " ";
+    }
     return 0; //----------> O(1)
 }
+
+// Calculating the time complexity:
+// 1. The first loop runs from 1 to n, which is O(n).
+// 2. The second loop runs from 1 to n/2, which is O(n/2) but simplifies to O(n).
+// 3. The third loop runs from 1 to n with a step of 2, which is also O(n/2) but simplifies to O(n).
+// 4. The input and output operations are O(1) each.
+// 5. The return statement is O(1).
+
+// Therefore, the overall time complexity is O(n) + O(n) + O(n) + O(1) + O(1), = O(n+n+n+1+1) = O(n). --> we always ignore constant factors and lower order terms in Big O notation.
 
 // The overall time complexity of this code is O(n).
