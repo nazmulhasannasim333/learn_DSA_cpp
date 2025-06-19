02f20867a022156bf6de441614094da39528b401
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 09:26:59 +0600

    Refactor vector example to demonstrate resizing and output elements

--- stl_vector/capacity.cpp
+++ stl_vector/capacity.cpp
@@ -16,20 +16,25 @@ int main()
     // v.push_back(10);
     // cout << v.capacity() << endl;
     v.push_back(10);
-    v.push_back(10);
-    v.push_back(10);
-    v.push_back(10);
-    cout << v.size() << endl; // Size is 4
-    v.clear();                // Clear the vector
-    cout << v.size() << endl; // Size is 0
-    cout << v[0] << endl;
-    if (v.empty()) // Check if the vector is empty
+    v.push_back(20);
+    v.push_back(30);
+    v.push_back(40);
+    // cout << v.size() << endl; // Size is 4
+    // v.clear();                // Clear the vector
+    // cout << v.size() << endl; // Size is 0
+    // cout << v[0] << endl;
+    // if (v.empty()) // Check if the vector is empty
+    // {
+    //     cout << "Vector is empty" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Vector is not empty" << endl;
+    // }
+    v.resize(2); // Resize the vector to 2 elements
+    for (int i = 0; i < v.size(); i++)
     {
-        cout << "Vector is empty" << endl;
-    }
-    else
-    {
-        cout << "Vector is not empty" << endl;
+        cout << v[i] << " "; // Print the elements of the vector
     }
 
     return 0;
