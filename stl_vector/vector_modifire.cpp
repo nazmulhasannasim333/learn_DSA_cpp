c322796a4edeeea1814fd87c1ac771a7443748fe
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 09:33:14 +0600

    Update vector output to include new element and adjust comments for clarity

--- stl_vector/vector_modifire.cpp
+++ stl_vector/vector_modifire.cpp
@@ -6,7 +6,8 @@ int main()
     vector<int> v = {1, 2, 3, 4, 5, 6};
     // vector<int> v2;
     // v2 = v;
-    v.pop_back(); // Remove the last element
+    v.pop_back();    // Remove the last element
+    v.push_back(10); // Add an element at the end
     for (int e : v)
     {
         cout << e << " ";
