2cf32dcc465cc273c4e5bfc9207bfa916db2ab24
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 10:54:29 +0600

    Update vector modification to include element search and output index

--- stl_vector/vector_modifire.cpp
+++ stl_vector/vector_modifire.cpp
@@ -13,7 +13,17 @@ int main()
     // v.insert(v.begin() + 2, v2.begin(), v2.end()); // Insert elements of v2 at index 2
     // v.erase(v.begin() + 3);       // Erase the element at index 3
     // v.erase(v.begin() + 1, v.begin() + 5); // Erase elements from index 2 to 4
-    replace(v.begin(), v.end() - 2, 3, 100); // Replace all occurrences of 3 with 100
+    // replace(v.begin(), v.end() - 2, 3, 100); // Replace all occurrences of 3 with 100
+    auto it = find(v.begin(), v.end(), 4);
+    if (it != v.end())
+    {
+        cout << "Element found at index: " << distance(v.begin(), it) << endl;
+    }
+    else
+    {
+        cout << "Element not found" << endl;
+    }
+
     for (int e : v)
     {
         cout << e << " ";
