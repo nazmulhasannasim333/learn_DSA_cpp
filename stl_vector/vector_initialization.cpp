c0ea00d7a03fbb94cac81adc32ebcd97671ff764
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 09:04:56 +0600

    Update vector initialization example by commenting out Type 5 and using a new initialization method

--- stl_vector/vector_initialization.cpp
+++ stl_vector/vector_initialization.cpp
@@ -9,7 +9,8 @@ int main()
     // vector<int> v(10, 3); // Type 3
     // vector<int> v2(v);    // Type 4
     int a[5] = {1, 2, 3, 4, 5};
-    vector<int> v(a, a + 5); // Type 5
+    // vector<int> v(a, a + 5); // Type 5
+    vector<int> v = {1, 2, 3, 4, 5, 6}; // Type 6
     for (int i = 0; i < v.size(); i++)
     {
         cout << v[i] << " ";
