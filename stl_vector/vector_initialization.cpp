14bbae44d496f88e8daf17ca2e60826256484a3a
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 08:56:12 +0600

    Update vector initialization example and output value

--- stl_vector/vector_initialization.cpp
+++ stl_vector/vector_initialization.cpp
@@ -3,7 +3,9 @@ using namespace std;
 
 int main()
 {
-    vector<int> v; // Type 1
+
+    // vector<int> v; // Type 1
+    vector<int> v(10); // Type 2
     cout << v.size() << endl;
     return 0;
 }
