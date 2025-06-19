b613eceadc8196d9c982712c9d3dd595b63456cb
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 09:32:39 +0600

    Remove unused vector and clean up output after element removal

--- stl_vector/vector_modifire.cpp
+++ stl_vector/vector_modifire.cpp
@@ -4,9 +4,10 @@ using namespace std;
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 5, 6};
-    vector<int> v2;
-    v2 = v;
-    for (int e : v2)
+    // vector<int> v2;
+    // v2 = v;
+    v.pop_back(); // Remove the last element
+    for (int e : v)
     {
         cout << e << " ";
     }
