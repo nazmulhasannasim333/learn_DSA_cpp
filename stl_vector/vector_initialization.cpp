33303780410fd18d98dc38426211927c62f1057c
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 09:01:10 +0600

    Update vector initialization values and adjust output logic

--- stl_vector/vector_initialization.cpp
+++ stl_vector/vector_initialization.cpp
@@ -6,10 +6,11 @@ int main()
 
     // vector<int> v; // Type 1
     // vector<int> v(10); // Type 2
-    vector<int> v(10, 5); // Type 3
-    for(int i = 0; i < v.size(); i++)
+    vector<int> v(10, 3); // Type 3
+    vector<int> v2(v);    // Type 4
+    for (int i = 0; i < v2.size(); i++)
     {
-        cout << v[i] << " ";
+        cout << v2[i] << " ";
     }
     return 0;
 }
