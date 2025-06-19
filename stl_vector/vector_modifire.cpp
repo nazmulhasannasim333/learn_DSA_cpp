985a92b86df0ce854ae5b63fab71e013607c08af
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 10:41:55 +0600

    Refactor vector modifications: comment out insertion of v2 elements and retain element removal logic

--- stl_vector/vector_modifire.cpp
+++ stl_vector/vector_modifire.cpp
@@ -10,8 +10,9 @@ int main()
     // v.pop_back();    // Remove the last element
     // v.push_back(10); // Add an element at the end
     // v.insert(v.begin() + 2, 100); // Insert 100 at index 2
-    v.insert(v.begin() + 2, v2.begin(), v2.end()); // Insert elements of v2 at index 2
+    // v.insert(v.begin() + 2, v2.begin(), v2.end()); // Insert elements of v2 at index 2
     // v.erase(v.begin() + 3);       // Erase the element at index 3
+    v.erase(v.begin() + 1, v.begin() + 5); // Erase elements from index 2 to 4
     for (int e : v)
     {
         cout << e << " ";
