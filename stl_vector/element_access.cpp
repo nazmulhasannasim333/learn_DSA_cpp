c19799e0f995bdf794fe7ec99ef883771595a172
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 11:30:24 +0600

    Refactor output and enhance clarity by uncommenting element access and updating output file

--- stl_vector/element_access.cpp
+++ stl_vector/element_access.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     vector<int> v = {1, 2, 3, 4, 5, 6};
-    cout << v[3] << endl;    // Accessing the 4th element using operator[]
-    cout << v.at(3) << endl; // Accessing the 4th element using at()
-    // cout << v.front() << endl; // Accessing the first element
+    cout << v[3] << endl;      // Accessing the 4th element using operator[]
+    cout << v.at(3) << endl;   // Accessing the 4th element using at()
+    cout << v.front() << endl; // Accessing the first element
     // cout << v.back() << endl;  // Accessing the last element
     return 0;
 }
