6c402a1a219ec026b6a5a8865b493b643ae95d12
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 12:10:32 +0600

    Fix input and output data for consistency; refactor vector input handling for clarity

--- stl_vector/vector_input.cpp
+++ stl_vector/vector_input.cpp
@@ -5,10 +5,22 @@ int main()
 {
     int n;
     cin >> n;
-    vector<int> v(n);
+    // vector<int> v(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> v[i];
+    // }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout << v[i] << " ";
+    // }
+
+    vector<int> v;
     for (int i = 0; i < n; i++)
     {
-        cin >> v[i];
+        int x;
+        cin >> x;
+        v.push_back(x);
     }
     for (int i = 0; i < n; i++)
     {
