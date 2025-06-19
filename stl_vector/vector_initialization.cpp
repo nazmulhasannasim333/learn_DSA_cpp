66aed5d9abaf4a34a4e91e952ba168a943341890
Nasim <mdnasimhosen333@gmail.com>
2025-06-19 09:04:17 +0600

    Update vector initialization example and output values

--- stl_vector/vector_initialization.cpp
+++ stl_vector/vector_initialization.cpp
@@ -6,11 +6,13 @@ int main()
 
     // vector<int> v; // Type 1
     // vector<int> v(10); // Type 2
-    vector<int> v(10, 3); // Type 3
-    vector<int> v2(v);    // Type 4
-    for (int i = 0; i < v2.size(); i++)
+    // vector<int> v(10, 3); // Type 3
+    // vector<int> v2(v);    // Type 4
+    int a[5] = {1, 2, 3, 4, 5};
+    vector<int> v(a, a + 5); // Type 5
+    for (int i = 0; i < v.size(); i++)
     {
-        cout << v2[i] << " ";
+        cout << v[i] << " ";
     }
     return 0;
 }
