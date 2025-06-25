3bbf6b0342259a8d9309c5977e412d4c92fe6a51
Nasim <mdnasimhosen333@gmail.com>
2025-06-25 20:42:45 +0600

    Add implementation for Insert It problem from HackerRank assignment

---
+++ Assignment 1/Insert_it.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int m;
+    cin >> m;
+    vector<int> arr2(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> arr2[i];
+    }
+
+    int x;
+    cin >> x;
+    for (int i = 0; i < x; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    for (int i = 0; i < m; i++)
+    {
+        cout << arr2[i] << " ";
+    }
+    for (int i = x; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
