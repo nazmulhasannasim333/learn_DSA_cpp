4b69afa427426a448dd89f09037085d59d9031da
Nasim <mdnasimhosen333@gmail.com>
2025-07-01 23:15:15 +0600

    Refactor code structure for improved readability and maintainability

---
+++ singly_linked_list_operations/insert_at_tail_optimized.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        return;
+    }
+
+    tail->next = new_node;
+    tail = new_node;
+}
+
+void print_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+
+    insert_at_tail(head, tail, 500);
+    insert_at_tail(head, tail, 40);
+    insert_at_tail(head, tail, 50);
+
+    print_list(head);
+
+    return 0;
+}
