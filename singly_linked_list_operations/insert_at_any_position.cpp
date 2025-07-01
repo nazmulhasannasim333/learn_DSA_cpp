f66b4db16027136a580a9ed0a11e11ea5cdc5472
Nasim <mdnasimhosen333@gmail.com>
2025-07-01 23:00:06 +0600

    Fix head initialization in main function to prevent null pointer dereference

--- singly_linked_list_operations/insert_at_any_position.cpp
+++ singly_linked_list_operations/insert_at_any_position.cpp
@@ -38,12 +38,16 @@ void print_list(Node *head)
 
 int main()
 {
-    Node *head = NULL;
+    Node *head = new Node(10);
     Node *a = new Node(20);
     Node *b = new Node(30);
+    Node *c = new Node(40);
+    Node *d = new Node(50);
 
     head->next = a;
     a->next = b;
+    b->next = c;
+    c->next = d;
 
     insert_at_any_position(head, 2, 500);
 
