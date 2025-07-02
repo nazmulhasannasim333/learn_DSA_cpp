0ec4372691f3427f5dbf9fba82f040af9b8bea5b
Nasim <mdnasimhosen333@gmail.com>
2025-07-03 01:05:32 +0600

    Refactor code structure for improved readability and maintainability

--- singly_linked_list_operations/sorting_linked_list.cpp
+++ singly_linked_list_operations/sorting_linked_list.cpp
@@ -27,10 +27,19 @@ void insert_at_tail(Node *&head, Node *&tail, int val)
     tail = new_node;
 }
 
-void sort_linked_list(Node *&head){
-
+void sort_linked_list(Node *&head)
+{
+    for (Node *i = head; i->next != NULL; i = i->next)
+    {
+        for (Node *j = i->next; j != NULL; j = j->next)
+        {
+            if (i->val > j->val)
+            {
+                swap(i->val, j->val);
+            }
+        }
+    }
 }
-
 
 void print_list(Node *head)
 {
