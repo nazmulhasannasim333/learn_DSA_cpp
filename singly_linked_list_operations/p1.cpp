223d896803490ce6eb8dc1b742b00ceaf7d4b4fa
Nasim <mdnasimhosen333@gmail.com>
2025-07-02 14:52:41 +0600

    Refactor code structure for improved readability and maintainability

---
+++ singly_linked_list_operations/p1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void print_list_size(Node *head)
+{
+    Node *temp = head;
+    int cnt = 0;
+    while (temp != NULL)
+    {
+        cnt++;
+        temp = temp->next;
+    }
+    cout << cnt << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+
+    head->next = a;
+    a->next = b;
+
+    print_list_size(head);
+
+    return 0;
+}
