22ba66add16a8f6f5ae19a849b020871b90fb339
Nasim <mdnasimhosen333@gmail.com>
2025-07-03 01:02:00 +0600

    Implement linked list operations including insertion and printing functionality

---
+++ singly_linked_list_operations/sorting_linked_list.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+
+    tail->next = new_node;
+    tail = new_node;
+}
+
+void sort_linked_list(Node *&head){
+
+}
+
+
+void print_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_at_tail(head, tail, val);
+    }
+
+    sort_linked_list(head);
+    print_list(head);
+
+    return 0;
+}
