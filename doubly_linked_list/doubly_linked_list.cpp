e14effcdace7c30cd84ad88ca1755a9434281313
Nasim <mdnasimhosen333@gmail.com>
2025-07-06 20:26:53 +0600

    Add initial implementation of doubly linked list with basic node structure

---
+++ doubly_linked_list/doubly_linked_list.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+
+    return 0;
+}
