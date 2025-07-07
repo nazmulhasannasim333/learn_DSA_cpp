92812bbbce69dc5f3ab3d7e96b43dc57b22b2236
Nasim <mdnasimhosen333@gmail.com>
2025-07-07 09:24:10 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ doubly_linked_list/insert_at_tail.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+
+    insert_at_tail(head, tail, 50);
+
+    print_forward(head);
+
+    return 0;
+}
