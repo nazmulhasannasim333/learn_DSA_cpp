c3c4cc32c8f1bad64cd56c70d720491cb82027d0
Nasim <mdnasimhosen333@gmail.com>
2025-07-07 12:01:43 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ doubly_linked_list/delete_at_any_position.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void delete_at_any_position(Node *&head, int pos)
+{
+    Node *temp = head;
+    for (int i = 1; i < pos; i++)
+    {
+        temp = temp->next;
+        if (temp == NULL)
+            return;
+    }
+    Node *delete_node = temp->next;
+    temp->next = delete_node->next;
+    delete_node->next->prev = temp;
+    delete delete_node;
+}
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head = new Node(10);
+    Node *a = new Node(20);
+    Node *b = new Node(30);
+    Node *tail = new Node(40);
+    head->next = a;
+    a->prev = head;
+
+    a->next = b;
+    b->prev = a;
+
+    b->next = tail;
+    tail->prev = b;
+
+    delete_at_any_position(head, 2);
+
+    print_forward(head);
+
+    return 0;
+}
