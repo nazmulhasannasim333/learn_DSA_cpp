3c4293cd68a9c5bdae45bc133ebcfa9d6a19d562
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 15:23:29 +0600

    Comment out unused list operations and streamline output for clarity

--- stl_list_and_cycle_detection/stl_modifires.cpp
+++ stl_list_and_cycle_detection/stl_modifires.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main()
 {
     list<int> l = {1, 2, 3, 4, 5, 6};
-    list<int> l2;
+    // list<int> l2;
     // l2 = l;
-    l2.assign(l.begin(), l.end());
-    for (int val : l2)
+    // l2.assign(l.begin(), l.end());
+    l.push_back(7);
+    l.push_front(100);
+    for (int val : l)
     {
         cout << val << " ";
     }
