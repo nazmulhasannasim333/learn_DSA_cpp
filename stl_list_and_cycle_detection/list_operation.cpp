c7313c5dff993e52ff6d75288424b9f67c008965
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 16:17:50 +0600

    Comment out sorting and uniqueness logic in list for clarity

--- stl_list_and_cycle_detection/list_operation.cpp
+++ stl_list_and_cycle_detection/list_operation.cpp
@@ -5,9 +5,9 @@ int main()
 {
     list<int> l = {10, 20, 30, 10, 50, 10};
     // l.remove(10);
-    l.sort();
+    // l.sort();
     // l.sort(greater<int>());
-    l.unique();
+    // l.unique();
     for (int val : l)
     {
         cout << val << " ";
