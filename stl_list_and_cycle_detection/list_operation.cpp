a52f2be7fb8b8eda01edb1bb9078960e1a8fa9e6
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 16:14:14 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ stl_list_and_cycle_detection/list_operation.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l = {10, 20, 30, 10, 50, 10};
+    // l.remove(10);
+    for (int val : l)
+    {
+        cout << val << " ";
+    }
+    return 0;
+}
