650612d70025451e2c72645013a4f2f55f0773b3
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 16:00:42 +0600

    Update output values and insert vector elements into the list

--- stl_list_and_cycle_detection/stl_modifires.cpp
+++ stl_list_and_cycle_detection/stl_modifires.cpp
@@ -5,6 +5,7 @@ int main()
 {
     list<int> l = {1, 2, 3, 4, 5, 6};
     list<int> l2 = {10, 20, 30, 40, 50, 60};
+    vector<int> v = {70, 80, 90};
     // list<int> l2;
     // l2 = l;
     // l2.assign(l.begin(), l.end());
@@ -15,6 +16,7 @@ int main()
     // cout << *next(l.begin(), 2) << endl;
     // l.insert(next(l.begin(), 2), 100);
     // l.insert(next(l.begin(), 2), l2.begin(), l2.end());
+    l.insert(next(l.begin(), 2), v.begin(), v.end());
 
     for (int val : l)
     {
