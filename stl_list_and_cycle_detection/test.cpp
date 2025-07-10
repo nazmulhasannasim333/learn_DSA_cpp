667f918d5fcafc9514260bd1e00d1032152d3340
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 15:05:04 +0600

    Comment out unused output statements for cleaner code

--- stl_list_and_cycle_detection/test.cpp
+++ stl_list_and_cycle_detection/test.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     list<int> l = {1, 2, 3, 4, 5};
-    cout << l.size() << endl;
-    cout << *l.begin() << endl;
-    list<int> l2(10, 3);
+    // cout << l.size() << endl;
+    // cout << *l.begin() << endl;
+    // list<int> l2(10, 3);
     for (int n : l)
     {
         cout << n << " ";
