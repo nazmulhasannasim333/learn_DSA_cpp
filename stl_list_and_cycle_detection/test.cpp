e2049ac5db659247ea7534a6df71c0fb0fdc82d3
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 15:12:36 +0600

    Update output message and comment out list clear operation for clarity

--- stl_list_and_cycle_detection/test.cpp
+++ stl_list_and_cycle_detection/test.cpp
@@ -9,7 +9,7 @@ int main()
     // int a[] = {10, 20, 30};
     // vector<int> v = {10, 20, 30, 40, 50, 60};
     // list<int> l2(v.begin(), v.end());
-    l.clear();
+    // l.clear();
     if (l.empty())
     {
         cout << "List is empty" << endl;
