6df7f22916b7194e2ad1079bb7232e207863c642
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 15:21:31 +0600

    Enhance VSCode settings and improve code formatting in stl_modifires.cpp

--- stl_list_and_cycle_detection/stl_modifires.cpp
+++ stl_list_and_cycle_detection/stl_modifires.cpp
@@ -1,11 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main()
+{
     list<int> l = {1, 2, 3, 4, 5, 6};
     list<int> l2;
-    l2 = l;
-    for(int val : l2){
+    // l2 = l;
+    l2.assign(l.begin(), l.end());
+    for (int val : l2)
+    {
         cout << val << " ";
     }
     return 0;
