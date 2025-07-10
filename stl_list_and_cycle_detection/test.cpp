5833b9e523d5c23575b3586fbe3a607f2627ca27
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 15:03:28 +0600

    Refactor code structure for improved readability and maintainability

---
+++ stl_list_and_cycle_detection/test.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    list<int> l = {1, 2, 3, 4, 5};
+    // cout << l.size() << endl;
+    // cout << *l.begin() << endl;
+    // list<int> l(10, 3);
+    // for (int n : l)
+    // {
+    //     cout << n << " ";
+    // }
+
+    
+
+    return 0;
+}
