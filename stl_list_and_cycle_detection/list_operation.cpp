6d5303c810add0606cb3358e359eb4d0af872a11
Nasim <mdnasimhosen333@gmail.com>
2025-07-10 16:17:21 +0600

    Refactor list sorting to remove descending order and ensure unique elements in output

--- stl_list_and_cycle_detection/list_operation.cpp
+++ stl_list_and_cycle_detection/list_operation.cpp
@@ -5,8 +5,9 @@ int main()
 {
     list<int> l = {10, 20, 30, 10, 50, 10};
     // l.remove(10);
-    // l.sort();
-    l.sort(greater<int>());
+    l.sort();
+    // l.sort(greater<int>());
+    l.unique();
     for (int val : l)
     {
         cout << val << " ";
