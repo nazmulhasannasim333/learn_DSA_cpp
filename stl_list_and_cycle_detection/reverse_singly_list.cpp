7ff1829ea97f4ca707ec2dea908925e21bc2a712
Nasim <mdnasimhosen333@gmail.com>
2025-07-11 15:05:53 +0600

    Refactor code structure for improved readability and maintainability

---
+++ stl_list_and_cycle_detection/reverse_singly_list.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+
+    tail->next = new_node;
+    tail = new_node;
+}
+
+void reverse_linked_list(Node *&head, Node *temp)
+{
+    if (temp->next == NULL)
+    {
+        head = temp;
+        return;
+    }
+    reverse_linked_list(head, temp->next);
+    temp->next->next = temp;
+    temp->next = NULL;
+}
+
+void print_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << endl;
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_at_tail(head, tail, val);
+    }
+
+    reverse_linked_list(head, head);
+
+    return 0;
+}
