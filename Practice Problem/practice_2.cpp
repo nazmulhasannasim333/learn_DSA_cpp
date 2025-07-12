3f4e6464e60aba8d6228838afedb47514aa08e1d
Nasim <mdnasimhosen333@gmail.com>
2025-07-12 11:27:34 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Practice Problem/practice_2.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void print_list(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+
+    int val1, val2;
+    while (cin >> val1 && val1 != -1)
+    {
+        insert_at_tail(head1, tail1, val1);
+    }
+
+    return 0;
+}
