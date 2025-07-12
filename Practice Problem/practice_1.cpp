3494c309471061de2198df763c9fd46bf970d5c3
Nasim <mdnasimhosen333@gmail.com>
2025-07-12 11:24:20 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ Practice Problem/practice_1.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void check_same(Node *head1, Node *head2)
+{
+    while (head1 != NULL && head2 != NULL)
+    {
+        if (head1->val != head2->val)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+        head1 = head1->next;
+        head2 = head2->next;
+    }
+    if (head1 == NULL && head2 == NULL)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    Node *head1 = NULL;
+    Node *tail1 = NULL;
+    Node *head2 = NULL;
+    Node *tail2 = NULL;
+
+    int val1, val2;
+    while (cin >> val1 && val1 != -1)
+    {
+        insert_at_tail(head1, tail1, val1);
+    }
+    while (cin >> val2 && val2 != -1)
+    {
+        insert_at_tail(head2, tail2, val2);
+    }
+    // check_same(head1, head2);
+
+    return 0;
+}
