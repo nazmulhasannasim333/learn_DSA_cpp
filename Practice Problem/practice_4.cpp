74f1a4b0c00705ecf70fd14c51da2d2e17f64eac
Nasim <mdnasimhosen333@gmail.com>
2025-07-12 14:39:11 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Practice Problem/practice_4.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+bool insert_at_pos(Node *&head, Node *&tail, int pos, int val)
+{
+    int count = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    if (pos < 0 || pos > count)
+    {
+        return false;
+    }
+}
+
+void print_forward(Node *head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void print_backward(Node *tail)
+{
+    Node *temp = tail;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->prev;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (t--)
+    {
+        int pos, val;
+        cin >> pos >> val;
+        bool result = insert_at_pos(head, tail, pos, val);
+        if (!result)
+        {
+            cout << "Invalid" << endl;
+            continue;
+        }
+        else
+        {
+            print_forward(head);
+            print_backward(tail);
+        }
+    }
+
+    return 0;
+}
