fc3f8bc79fba7db335a8baea925ff861fdc0aacb
Nasim <mdnasimhosen333@gmail.com>
2025-07-16 21:08:17 +0600

    Add implementation for Remove Duplicate Again problem with linked list

---
+++ Mid Term/Remove_Duplicate_Again.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *prev;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->prev = NULL;
+        this->next = NULL;
+    }
+};
+
+void insert_at_tail(Node *&head, Node *&tail, int val)
+{
+    Node *new_node = new Node(val);
+    if (head == NULL)
+    {
+        head = new_node;
+        tail = new_node;
+        return;
+    }
+    tail->next = new_node;
+    new_node->prev = tail;
+    tail = new_node;
+}
+
+void remove_duplicate(Node *&head)
+{
+    Node *tmp = head;
+    list<int> values;
+    while (tmp != NULL)
+    {
+        values.push_back(tmp->val);
+        tmp = tmp->next;
+    }
+
+    values.sort();
+    values.unique();
+
+    for (int value : values)
+    {
+        cout << value << " ";
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int val;
+    while (true)
+    {
+        cin >> val;
+        if (val == -1)
+            break;
+        insert_at_tail(head, tail, val);
+    }
+
+    remove_duplicate(head);
+
+    return 0;
+}
