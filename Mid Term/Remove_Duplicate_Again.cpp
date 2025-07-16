f9b7f59ebd934cd3575eb5c26b0d7c574eb8cc71
Nasim <mdnasimhosen333@gmail.com>
2025-07-16 21:12:21 +0600

    Refactor Remove Duplicate logic to use linked list and implement insert_at_tail function

--- Mid Term/Remove_Duplicate_Again.cpp
+++ Mid Term/Remove_Duplicate_Again.cpp
@@ -1,44 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Node
+int main()
 {
-public:
-    int val;
-    Node *prev;
-    Node *next;
-    Node(int val)
-    {
-        this->val = val;
-        this->prev = NULL;
-        this->next = NULL;
-    }
-};
-
-void insert_at_tail(Node *&head, Node *&tail, int val)
-{
-    Node *new_node = new Node(val);
-    if (head == NULL)
-    {
-        head = new_node;
-        tail = new_node;
-        return;
-    }
-    tail->next = new_node;
-    new_node->prev = tail;
-    tail = new_node;
-}
-
-void remove_duplicate(Node *&head)
-{
-    Node *tmp = head;
     list<int> values;
-    while (tmp != NULL)
+    int val;
+    while (true)
     {
-        values.push_back(tmp->val);
-        tmp = tmp->next;
+        cin >> val;
+        if (val == -1)
+            break;
+        values.push_back(val);
     }
-
     values.sort();
     values.unique();
 
@@ -46,23 +19,6 @@ void remove_duplicate(Node *&head)
     {
         cout << value << " ";
     }
-}
-
-int main()
-{
-    Node *head = NULL;
-    Node *tail = NULL;
-
-    int val;
-    while (true)
-    {
-        cin >> val;
-        if (val == -1)
-            break;
-        insert_at_tail(head, tail, val);
-    }
-
-    remove_duplicate(head);
-
+    cout << endl;
     return 0;
 }
