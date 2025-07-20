4629d12b14bf3b6214c7594738d2f835ab0620fc
Nasim <mdnasimhosen333@gmail.com>
2025-07-20 23:00:58 +0600

    Implement stack functionality using list with push, pop, top, empty, and size methods

---
+++ stack_implementation_and_STL/implement_stack_using_list.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Stack
+{
+public:
+    list<int> l;
+
+    void push(int val)
+    {
+        v.push_back(val);
+    }
+    void pop()
+    {
+        v.pop_back();
+    }
+    int top()
+    {
+        return v.back();
+    }
+    bool empty()
+    {
+        return v.empty();
+    }
+    int size()
+    {
+        return v.size();
+    }
+};
+
+int main()
+{
+    Stack s;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.push(x);
+    }
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
