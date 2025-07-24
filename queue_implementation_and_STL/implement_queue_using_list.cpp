83ccf384092e391c5538562bb439da6772038412
Nasim <mdnasimhosen333@gmail.com>
2025-07-24 09:50:20 +0600

    Refactor code structure for improved readability and maintainability

--- queue_implementation_and_STL/implement_queue_using_list.cpp
+++ queue_implementation_and_STL/implement_queue_using_list.cpp
@@ -34,6 +34,18 @@ public:
 int main()
 {
     myQueue q;
-
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        q.push(x);
+    }
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
     return 0;
 }
