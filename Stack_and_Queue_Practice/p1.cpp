b8858882b14e09fb56a9404d8fbe16d99c67f43e
Nasim <mdnasimhosen333@gmail.com>
2025-07-25 09:56:16 +0600

    Implement code changes to enhance functionality and improve performance

--- Stack_and_Queue_Practice/p1.cpp
+++ Stack_and_Queue_Practice/p1.cpp
@@ -1,7 +1,79 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+class MyStack
+{
+public:
+    list<int> l;
+
+    void push(int x)
+    {
+        l.push_back(x);
+    }
+    void pop()
+    {
+        l.pop_back();
+    }
+    int top()
+    {
+        return l.back();
+    }
+    int size()
+    {
+        return l.size();
+    }
+    bool empty()
+    {
+        return l.empty();
+    }
+};
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    MyStack s;
+    MyStack s2;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.push(x);
+    }
+    cin >> m;
+    for (int i = 0; i < m; i++)
+    {
+        int x;
+        cin >> x;
+        s2.push(x);
+    }
+
+    bool isEqual = true;
+    if (s.size() != s2.size())
+    {
+        isEqual = false;
+    }
+    else
+    {
+        while (!s.empty() && !s2.empty())
+        {
+            if (s.top() != s2.top())
+            {
+                isEqual = false;
+                break;
+            }
+            s.pop();
+            s2.pop();
+        }
+    }
+    if (isEqual)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
     return 0;
 }
