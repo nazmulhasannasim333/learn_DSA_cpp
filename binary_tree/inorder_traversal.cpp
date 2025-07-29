54b5cb0c42d1f689d6ef18dee6581a638b2e5acf
Nasim <mdnasimhosen333@gmail.com>
2025-07-29 09:42:17 +0600

    Refactor code structure for improved readability and maintainability

---
+++ binary_tree/inorder_traversal.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+void inorderTraversal(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    inorderTraversal(root->left);
+    cout << root->val << " ";
+    inorderTraversal(root->right);
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    Node *a = new Node(7);
+    Node *b = new Node(9);
+    Node *c = new Node(2);
+    Node *d = new Node(6);
+    Node *e = new Node(9);
+    Node *f = new Node(5);
+    Node *g = new Node(11);
+    Node *h = new Node(5);
+
+    root->left = a;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    b->left = e;
+    d->left = f;
+    d->right = g;
+    e->right = h;
+
+    inorderTraversal(root);
+
+    return 0;
+}
