ed487025bf652fc8b4395073de46f9d216ee899d
Nasim <mdnasimhosen333@gmail.com>
2025-07-29 08:53:18 +0600

    Refactor code structure for improved readability and maintainability

--- binary_tree/preorder_traversal.cpp
+++ binary_tree/preorder_traversal.cpp
@@ -15,8 +15,38 @@ public:
     }
 };
 
+void preorderTraversal(Node *root)
+{
+    if (root == NULL)
+        return;
+
+    cout << root->val << " ";
+    preorderTraversal(root->left);
+    preorderTraversal(root->right);
+}
+
 int main()
 {
-    Node *root = new Node(10);
+    Node *root = new Node(1);
+    Node *a = new Node(7);
+    Node *b = new Node(2);
+    Node *c = new Node(6);
+    Node *d = new Node(5);
+    Node *e = new Node(11);
+    Node *f = new Node(9);
+    Node *g = new Node(9);
+    Node *h = new Node(5);
+
+    root->left = a;
+    a->left = b;
+    a->right = c;
+    c->left = d;
+    c->right = e;
+    root->right = f;
+    f->left = g;
+    g->left = h;
+
+    preorderTraversal(root);
+
     return 0;
 }
