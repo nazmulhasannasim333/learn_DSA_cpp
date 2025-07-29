597922a9c99207003c7e4a4372887be461eefa7e
Nasim <mdnasimhosen333@gmail.com>
2025-07-29 09:00:21 +0600

    Rearrange node initialization and connections for correct binary tree structure

--- binary_tree/preorder_traversal.cpp
+++ binary_tree/preorder_traversal.cpp
@@ -29,23 +29,22 @@ int main()
 {
     Node *root = new Node(1);
     Node *a = new Node(7);
-    Node *b = new Node(2);
-    Node *c = new Node(6);
-    Node *d = new Node(5);
-    Node *e = new Node(11);
-    Node *f = new Node(9);
-    Node *g = new Node(9);
+    Node *b = new Node(9);
+    Node *c = new Node(2);
+    Node *d = new Node(6);
+    Node *e = new Node(9);
+    Node *f = new Node(5);
+    Node *g = new Node(11);
     Node *h = new Node(5);
 
     root->left = a;
-    a->left = b;
-    a->right = c;
-    c->left = d;
-    c->right = e;
-
-    root->right = f;
-    f->left = g;
-    g->left = h;
+    root->right = b;
+    a->left = c;
+    a->right = d;
+    b->left = e;
+    d->left = f;
+    d->right = g;
+    e->right = h;
 
     preorderTraversal(root);
 
