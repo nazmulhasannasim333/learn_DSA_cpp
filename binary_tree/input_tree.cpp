cd8336dc4eab81bd8bc7f3b688943e2aa9a7be5e
Nasim <mdnasimhosen333@gmail.com>
2025-07-29 23:28:54 +0600

    Fix tree input handling to correctly initialize root and queue

--- binary_tree/input_tree.cpp
+++ binary_tree/input_tree.cpp
@@ -43,9 +43,14 @@ Node *inputTree()
 {
     int val;
     cin >> val;
-    Node *root = new Node(val);
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
     queue<Node *> q;
-    q.push(root);
+    if (root)
+        q.push(root);
 
     while (!q.empty())
     {
