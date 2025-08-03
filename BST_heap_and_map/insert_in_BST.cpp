300e8a2f2bd7959d476d0a65624c6222746691d3
Nasim <mdnasimhosen333@gmail.com>
2025-08-03 22:29:00 +0600

    Refactor code structure for improved readability and maintainability

---
+++ BST_heap_and_map/insert_in_BST.cpp
@@ -0,0 +1,106 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node(int val)
+    {
+        this->val = val;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+Node *inputTree()
+{
+    int val;
+    cin >> val;
+    Node *root;
+    if (val == -1)
+        root = NULL;
+    else
+        root = new Node(val);
+    queue<Node *> q;
+    if (root)
+        q.push(root);
+
+    while (!q.empty())
+    {
+        Node *p = q.front();
+        q.pop();
+
+        int l, r;
+        cin >> l >> r;
+
+        Node *leftChild, *rightChild;
+        if (l == -1)
+            leftChild = NULL;
+        else
+            leftChild = new Node(l);
+
+        if (r == -1)
+            rightChild = NULL;
+        else
+            rightChild = new Node(r);
+
+        p->left = leftChild;
+        p->right = rightChild;
+
+        if (p->left)
+            q.push(leftChild);
+        if (p->right)
+            q.push(rightChild);
+    }
+    return root;
+}
+void levelOrderTraversal(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "Tree is empty." << endl;
+        return;
+    }
+    queue<Node *> q;
+    q.push(root);
+
+    while (!q.empty())
+    {
+        Node *f = q.front();
+        q.pop();
+
+        cout << f->val << " ";
+
+        if (f->left)
+            q.push(f->left);
+
+        if (f->right)
+            q.push(f->right);
+    }
+}
+
+void insert(Node *&root, int val)
+{
+    if (root == NULL)
+    {
+        root = new Node(val);
+        return;
+    }
+    if (val < root->val)
+        insert(root->left, val);
+    else
+        insert(root->right, val);
+}
+
+int main()
+{
+    Node *root = inputTree();
+    int val;
+    cin >> val;
+    insert(root, val);
+    levelOrderTraversal(root);
+    return 0;
+}
