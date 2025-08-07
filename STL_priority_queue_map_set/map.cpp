7ddf22f2e040210934ce4b4e8afa92d97a0c879c
Nasim <mdnasimhosen333@gmail.com>
2025-08-07 14:10:19 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ STL_priority_queue_map_set/map.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    map<string, int> mp;
+    mp["Nasim"] = 90;
+    mp["Nazmul"] = 85;
+    mp["Hasan"] = 90;
+    mp["Rafiq"] = 95;
+    for (auto it : mp)
+    {
+        cout << it.first << " " << it.second << endl;
+    }
+    // if (mp["Nasim"])
+    //     cout << "Found Nasim with marks: " << mp["Nasim"] << endl;
+    // else
+    //     cout << "Nasim not found" << endl;
+    return 0;
+}
