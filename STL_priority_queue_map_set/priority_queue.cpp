ad1ba3cf1cd1110806982b6ee784443aaee25334
Nasim <mdnasimhosen333@gmail.com>
2025-08-07 08:24:37 +0600

    Update priority queue implementation and output file

--- STL_priority_queue_map_set/priority_queue.cpp
+++ STL_priority_queue_map_set/priority_queue.cpp
@@ -1,13 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    priority_queue<int> pq;
+int main()
+{
+    // priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
     pq.push(10);
     pq.push(20);
     pq.push(15);
     pq.push(30);
-    pq.push(5);
+    pq.push(2);
 
     cout << pq.top() << endl;
     cout << pq.size() << endl;
