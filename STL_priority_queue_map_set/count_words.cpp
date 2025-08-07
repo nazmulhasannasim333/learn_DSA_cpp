5830f9f4bf57fe93066f5940f0972c27dc74c99a
Nasim <mdnasimhosen333@gmail.com>
2025-08-07 14:17:03 +0600

    Refactor code structure for improved readability and maintainability

---
+++ STL_priority_queue_map_set/count_words.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+    stringstream ss(s);
+    string word;
+    map<string, int> wordCount;
+    while (ss >> word)
+    {                      // O(NlogN)
+        wordCount[word]++; // O(logN)
+    }
+    for (auto it : wordCount)
+    {                                                 // O(NlogN)
+        cout << it.first << " " << it.second << endl; 
+    }
+    return 0;
+}
