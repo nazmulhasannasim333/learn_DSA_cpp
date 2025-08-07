2a78a9b1e32c546b022e3ae7abf3da9915276fc0
Nasim <mdnasimhosen333@gmail.com>
2025-08-07 08:22:20 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ STL_priority_queue_map_set/priority_queue.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    priority_queue<int> pq;
+    pq.push(10);
+    pq.push(20);
+    pq.push(15);
+    pq.push(30);
+    pq.push(5);
+
+    cout << pq.top() << endl;
+    return 0;
+}
